8222f6494509ecadcd3235b1dc6bb360e56683fd
fergusrainniansangalang-lab <fergusrainnian.sangalang@letran.edu.ph>
2026-03-28 16:51:15 +0800

    Modified Move Smart Contract Portfolio

--- portfolio_frontend/src/views/PortfolioView.tsx
+++ portfolio_frontend/src/views/PortfolioView.tsx
@@ -98,7 +98,7 @@ const PortfolioView = () => {
   // ==========================================================================
   // STATE MANAGEMENT
   // ==========================================================================
-  const objectId = "0xa3343391df96e28464499f4c209d51bf209c07392fdeea97bfeee59e7550f020";
+  const objectId = "0x3a689e1569466f00e501df57d6f1688f55085c7c1b37e15e1cdc8dda4cd0fbbc";
   
   // Network state - default to testnet, can be changed if needed
   const [currentNetwork, setCurrentNetwork] = useState<"testnet" | "mainnet">("mainnet");
